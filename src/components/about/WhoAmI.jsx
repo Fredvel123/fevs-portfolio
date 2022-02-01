088074f659eb26f3c0022593750829fc39c824cb
Freddy <freddyvelarde59@gmail.com>
2022-02-01 08:55:56 -0400

    I finished the about page

--- src/components/about/WhoAmI.jsx
+++ src/components/about/WhoAmI.jsx
@@ -5,7 +5,10 @@ import profile  from '../../images/wallpaper6.jpg'
 import { Div, Info } from '../../styles/about/whoAmI'
 
 function WhoAmI() {
-  const element = useRef(null);
+  const paragraph1 = useRef(null);
+  const paragraph2 = useRef(null);
+  const image = useRef(null);
+  const button = useRef(null);
   //
   const callback = observer => {
     observer.forEach(observe => {
@@ -18,38 +21,41 @@ function WhoAmI() {
   }
   const options = {
     root: null,
-    rootMargin: '0px',
+    rootMargin: '10% 0px -10% 0px',
     threshold: 0.5
   }
   useEffect(() => {
     const observer = new IntersectionObserver(callback, options);
-    observer.observe(element.current)
-  }, []);
+    observer.observe(paragraph1.current)
+    observer.observe(paragraph2.current)
+    observer.observe(image.current)
+    observer.observe(button.current)
+    // eslint-disable-next-line
+  }, []);  
   
-
   return (
     <Div>
       <Info>
-        <div ref={element} className='section'>
+        <div ref={paragraph1} className='section'>
           <h2>Who am I</h2>
           <h3>
             I'm Freddy Velarde, 20 years old and I'm from Bolivia.
-            I'm a programmer and web developer front-end, Everything I learned are self-taught
+            I'm a programmer and front-end web developer, Everything I learned are as self-taught learner,
             over many mounths and throght many projects made.
           </h3>
         </div>
-        <div ref={element} className='section'>
+        <div ref={paragraph2} className='section'>
           <h2>Skills & tools</h2>
           <h3>
-            Software development: Git(version controller), terminal/linux, english/b1,  
+            <span>Software development:</span> Git(version controller), terminal/linux, english(b-1).  
           </h3>
           <h3>
-            front-end: Html, css/responsive-design, javascript, ReactJs Redux/ToolkIt, Hooks, styled-components, react-router,   
+            <span>Front-end:</span> Html, css/responsive-design, javascript, ReactJs, Redux/ToolkIt, styled-components, react-router.   
           </h3>
         </div>
-        <a ref={element}  href="#contact">Get in touch</a>
+        <a ref={button}  href="#contact">Get in touch</a>
       </Info>
-      <img ref={element} src={profile} width='50px' alt="" />
+      <img ref={image} src={profile} width='50px' alt="" />
     </Div>
   );
 }
--- src/styles/about/whoAmI.jsx
+++ src/styles/about/whoAmI.jsx
@@ -64,24 +64,28 @@ export const Info = styled.div`
       line-height: 5vh;
       font-weight: 50;
       margin-bottom: 2.4vh;
+      span {
+        color: #cccccc;
+        font-weight: 80;
+        font-family: ${fonts.letter1};
+      }
     }
   }
   
   a {
     opacity: 0;
-    transition: ease-in-out .7s;
-    transform: translateY(-10%);
     color: white;
     font-family: ${fonts.letter2};
     background: ${colors.darkPink};
     padding: 3vh 5%;
-    transition: ease-in .3s;
+    transition: ease-in 1.2s;
+    scale: 0.5;
     &:hover{
       background: ${colors.secondBlue};
     }
     &.visible {
+      scale: 1;
       opacity: 1;
-      transform: translateY(0);
     }
   }
   div.visible{
